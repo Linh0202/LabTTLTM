8bb6010690ec8bdae694cfb95cdb4056d0a991b7
Linh Nguyễn <53852807+Linh0202@users.noreply.github.com>
2021-05-20 15:55:25 +0700

    Add files via upload

---
+++ lab11/src/bai7/URLReader.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai7;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author linhn
+ */
+
+public class URLReader {
+
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://www.oracle.com/");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+}
